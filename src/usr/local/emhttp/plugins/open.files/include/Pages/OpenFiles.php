51fda4658b4c2e0d0d3e38fd4a274e100b11930c
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2025-06-22 14:57:52 +0000

    chore: update license header

--- src/usr/local/emhttp/plugins/open.files/include/Pages/OpenFiles.php
+++ src/usr/local/emhttp/plugins/open.files/include/Pages/OpenFiles.php
@@ -4,15 +4,22 @@ namespace EDACerton\OpenFiles;
 
 use EDACerton\PluginUtils\Translator;
 
-/* Copyright 2015-2025, Dan Landon.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License version 2,
- * as published by the Free Software Foundation.
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- */
+/*
+    Copyright (C) 2025  Derek Kaser
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 if ( ! defined(__NAMESPACE__ . '\PLUGIN_ROOT') || ! defined(__NAMESPACE__ . '\PLUGIN_NAME')) {
     throw new \RuntimeException("Common file not loaded.");
